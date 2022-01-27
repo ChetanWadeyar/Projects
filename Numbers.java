c9cd144d7ce1b3086f9eaffcbb74397803a1b98f
ChetanWadeyar <98532458+ChetanWadeyar@users.noreply.github.com>
2022-01-27 21:11:52 +0530

    Add files via upload

---
+++ Numbers.java
@@ -0,0 +1,21 @@
+class  Numbers
+{
+	public static void main(String[] args) 
+
+	{
+		int num=50;
+		if (num==0)
+		{
+			System.out.println("num is zero");
+		}
+		else if (num>0)
+		{
+			System.out.println("positive number");
+		}
+		else
+		{
+			System.out.println("negative number");
+		}
+	
+	}
+}
