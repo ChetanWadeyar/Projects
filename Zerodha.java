912acba9258dca8183c9d608472731810a72202e
ChetanWadeyar <98532458+ChetanWadeyar@users.noreply.github.com>
2022-01-27 21:20:26 +0530

    Add files via upload

---
+++ Zerodha.java
@@ -0,0 +1,28 @@
+class Zerodha 
+{
+	public static void main(String[] args) 
+	{
+		String username="chetan"; 
+		String password="c123";
+		if (username=="chetan" && password=="c123")
+			{
+			System.out.println("enter authentic password");
+			
+			int authenticationpassword=456789;
+			if (authenticationpassword==456789)
+				{
+				System.out.println("login successfull");
+				}
+				else 
+				{
+				System.out.println("wrong access");
+				}
+			}
+
+	else 
+	{
+	System.out.println("get lost");
+	}
+		
+	}
+}
